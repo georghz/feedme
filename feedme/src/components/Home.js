77704e5b3836733ce294b1eca51b85af0bfa2c4e
matskl <matskl@stud.ntnu.no>
2022-02-14 22:31:06 +0100

    Finished like, diveded Home.js into components. Finished like functionality with like/unlike Diveded code from Home.js into "RecipeLike.js" and "DeleteRecipe.js" Fixed bug with firebase usage

--- feedme/src/components/Home.js
+++ feedme/src/components/Home.js
@@ -1,53 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { getDocs, collection, deleteDoc, doc } from "firebase/firestore";
-import { auth, db } from "../firebase-config";
+import { getDocs, collection } from "firebase/firestore";
+import { db, auth } from "../firebase-config";
 import "./Home.css";
+import "./CreatePost"; 
+import DeleteRecipe from "./DeleteRecipe";
+import RecipeLike from "./RecipeLike";
 
 function Home({ isAuth }) {
   const [postLists, setPostList] = useState([]);
   const postsCollectionRef = collection(db, "posts");
 
-  const deletePost = async (id) => {
-    const postDoc = doc(db, "posts", id);
-    await deleteDoc(postDoc);
-  };
+  const getPosts = async () => {
+    const data = await getDocs(postsCollectionRef);
+    setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  }
 
   useEffect(() => {
-    const getPosts = async () => {
-      const data = await getDocs(postsCollectionRef);
-      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
     getPosts();
-  }, [deletePost]);
-
+  }, []);  
+  
   return (
     <div className="homePage">
       {postLists.map((post) => {
         return (
-          <div className="post">
+          <div className="post" key={post.id}>
             <div className="postHeader">
               <div className="title">
                 <h1> {post.title}</h1>
               </div>
-              <div className="deletePost">
-                {isAuth && post.author.id === auth.currentUser.uid && (
-                  <button
-                    onClick={() => {
-                      deletePost(post.id);
-                    }}
-                  >
-                    {" "}
-                    &#128465;
-                  </button>
-                )}
-              </div>
+              <DeleteRecipe post={post} isAuth={isAuth} triggerUpdate={getPosts}/>
             </div>
             <b>Ingredients: </b>
             <div className="postIngredientContainer"> {post.steps} </div>
             <b> Steps: </b>
             <div className="postTextContainer"> {post.postText} </div>
             <h3>@{post.author.name}</h3>
+            <RecipeLike post={post}  triggerUpdate={getPosts} />
           </div>
         );
       })}
