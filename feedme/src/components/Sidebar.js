e222a5b61520718d142ae04cff5e058342efc28f
matskl <matskl@stud.ntnu.no>
2022-03-17 15:54:06 +0100

    Working more with filter function Decent progress
    Co-authored-by: Anine Syrstad <aninesy@ntnu.no>

--- feedme/src/components/Sidebar.js
+++ feedme/src/components/Sidebar.js
@@ -1,10 +1,36 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
 
-export default function Sidebar() {
+export default function Sidebar({ recipesList, setRecipeList }) {
+  const [dinnerChecked, setDinnerChecked] = React.useState(false);
+
+  const handleDinnerChange = () => {
+    console.log(recipesList);
+    setDinnerChecked(!dinnerChecked);
+    setRecipeList([]);
+    // recipesList = [];
+    // console.log(recipesList)
+    // console.log("anine");
+  };
+
+  const Checkbox = ({ label, value, onChange }) => {
     return (
-        <div className="hei">
-            Dette er en sidebar
-        </div>
-    )
+      <label>
+        <input type="checkbox" checked={value} onChange={onChange} />
+        {label}
+      </label>
+    );
+  };
+
+  return (
+    <div className="hei">
+      <div>
+        <Checkbox
+          label="Dinner"
+          value={dinnerChecked}
+          onChange={handleDinnerChange}
+        />
+      </div>
+    </div>
+  );
 }
--- feedme/src/pages/Recipes.js
+++ feedme/src/pages/Recipes.js
@@ -1,13 +1,10 @@
-
 import "./Recipes.css";
 import React, { useEffect, useState } from "react";
 import { getDocs, collection } from "firebase/firestore";
 import { db, auth } from "../firebase-config";
 import Recipe from "../components/Recipe";
 import Sidebar from "../components/Sidebar";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
 
 function Recipes() {
   const [recipeLists, setRecipeList] = useState([]);
@@ -16,19 +13,17 @@ function Recipes() {
   const getRecipes = async () => {
     const data = await getDocs(recipesCollectionRef);
     setRecipeList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-  }
+  };
 
   useEffect(() => {
     getRecipes();
-  }, []);  
+  }, []);
 
   return (
     <div className="homePage">
-       <Sidebar/>
+      <Sidebar recipeLists={recipeLists} setRecipeList={setRecipeList} />
       {recipeLists.map((recipe) => {
-        return (
-          <Recipe recipe={recipe} triggerUpdate={getRecipes}/>
-        );
+        return <Recipe recipe={recipe} triggerUpdate={getRecipes} />;
       })}
     </div>
   );
