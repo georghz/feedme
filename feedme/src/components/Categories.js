440bbe780592980e173a1100ac4c3efdfd639fdc
matskl <matskl@stud.ntnu.no>
2022-03-18 22:31:13 +0100

    Setting categories on recipes working as it should Not fixed  showing filtered recipes yet

--- feedme/src/components/Categories.js
+++ feedme/src/components/Categories.js
@@ -1,91 +1,43 @@
 import React, { useState } from "react";
 
 export default function Categories({ categoriesList, setCategoryList }) {
-  //Working code
-
-  const [dinnerChecked, setDinnerChecked] = React.useState(false);
-  const [breakfastChecked, setBreakfastChecked] = React.useState(false);
-  const [categoriesChecked, setCategoriesChecked] = useState([]);
-
-  const handleDinnerChange = () => {
-    setDinnerChecked(!dinnerChecked);
-    if (!dinnerChecked) {
-      categoriesList.push("dinner");
-    } else {
-      const index = categoriesList.indexOf("dinner");
-      categoriesList.splice(index, 1);
-    }
-  };
-
-  const handleBreakfastChecked = () => {
-    setBreakfastChecked(!breakfastChecked);
-    if (!breakfastChecked) {
-      categoriesList.push("breakfast");
-    } else {
-      const index = categoriesList.indexOf("breakfast");
-      categoriesList.splice(index, 1);
-    }
-  };
-
-  const Checkbox = ({ label, value, onChange }) => {
-  return (
-    <label>
-      <input type="checkbox" checked={value} onChange={onChange} />
-      {label}
-    </label>
-  );
-};
-
-  return (
-    <div>
-      <Checkbox
-        label="Dinner"
-        value={dinnerChecked}
-        onChange={handleDinnerChange}
-      />
-      <br />
-      <Checkbox
-        label="Breakfast"
-        value={breakfastChecked}
-        onChange={handleBreakfastChecked}
-      />
-    </div>
-  );
-}
-
-/*
-  // Not working, but trying to get this to work. This code is on a cleaner format
-
   const recipeCategory = {
     dinner: "dinner",
     breakfast: "breakfast",
-    // vegan, gluten free ++
+    // vegan: "vegan",
+    // glutenFree: "gluten free",
   };
 
-  const handleCheck = (checked, recipeCategory) => {
+  const updateCategory = (checked, recipeCategory) => {
     if (checked) {
       setCategoryList([...categoriesList, recipeCategory]);
-    } else { // unchecked
-      const index = categoriesList.indexOf(recipeCategory);
-      setCategoryList(categoriesList.splice(index, 1));
+    } else {
+      // unchecked
+      const indexToBeRemoved = categoriesList.indexOf(recipeCategory);
+      categoriesList.splice(indexToBeRemoved, 1);
     }
   };
 
-  const Checkbox = ({ label, value, onChange }) => {
-    return (
-      <label>
-        <input type="checkbox" checked={value} onChange={onChange} />
-        {label}
-      </label>
-    );
-  };
-  
-return (
-    recipeCategory.map(recipeCategory => (
-      <Checkbox
-        label={recipeCategory}
-        onChange={(value) => handleCheck(value, recipeCategory)}
-      /> ))
-  );
-  */
+  return Object.values(recipeCategory).map((recipeCategory) => (
+    <CategoryCheckbox
+      label={recipeCategory}
+      handleCheck={(value) => updateCategory(value, recipeCategory)}
+    />
+  ));
+}
 
+function CategoryCheckbox({ label, handleCheck }) {
+  const [checked, setChecked] = useState(false);
+
+  const onClick = () => {
+    setChecked(!checked);
+    handleCheck(!checked);
+  };
+
+  return (
+    <label>
+      <input type="checkbox" value={checked} onClick={onClick} key={label} />
+      {label}
+    </label>
+  );
+}
--- feedme/src/components/Sidebar.js
+++ feedme/src/components/Sidebar.js
@@ -1,36 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getDocs, collection, query, where } from "firebase/firestore";
+import { db, auth } from "../firebase-config";
+import { useContext } from "react";
+import { AuthContext } from "../App";
 import "./Sidebar.css";
+import Categories from "./Categories";
 
 export default function Sidebar({ recipesList, setRecipeList }) {
-  const [dinnerChecked, setDinnerChecked] = React.useState(false);
+ // const [dinnerChecked, setDinnerChecked] = React.useState(false);
+  const [categories, setCategories] = useState([]);
+
+  const user = useContext(AuthContext);
 
   const handleDinnerChange = () => {
-    console.log(recipesList);
-    setDinnerChecked(!dinnerChecked);
-    setRecipeList([]);
+
+    //setDinnerChecked(!dinnerChecked);
+    getMyPosts();
     // recipesList = [];
     // console.log(recipesList)
     // console.log("anine");
+  }; 
+
+  const hei = () => {
+    console.log("Hei på deg");
+  }
+
+  const getMyPosts = async () => {
+    console.log("Nå kjører dinnerchange")
+    
+   // console.log(Object.values(categories));
+  /*  Object.values(categories).forEach(category => {
+      console.log(category);
+      const q = query(
+        collection(db, "recipes"),
+        where("categories", "array-contains", category)
+      );
+      const data =  getDocs(q);
+      setRecipeList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    });*/
+    const q = query(
+      collection(db, "recipes"),
+      where("categories", "array-contains", "dinner")
+    );
+    const data = await getDocs(q);
+    setRecipeList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+
+    
   };
 
-  const Checkbox = ({ label, value, onChange }) => {
-    return (
-      <label>
-        <input type="checkbox" checked={value} onChange={onChange} />
-        {label}
-      </label>
-    );
-  };
+  useEffect(() => {
+    getMyPosts(); 
+  }, []);
+
 
   return (
-    <div className="hei">
-      <div>
-        <Checkbox
-          label="Dinner"
-          value={dinnerChecked}
-          onChange={handleDinnerChange}
-        />
-      </div>
+    <div className="sidebar">
+      <Categories categoriesList={categories} setCategoryList={setCategories}/>
     </div>
   );
 }
